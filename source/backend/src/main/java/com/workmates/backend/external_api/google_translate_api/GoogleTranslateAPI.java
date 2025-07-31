c596ae4a78c4d217b32fa50c642cd97e23f58475
unknown <hwshin0727@gmail.com>
2025-07-31 14:37:11 +0900

    google translate api - getTranslatedText 작성

--- source/backend/src/main/java/com/workmates/backend/external_api/google_translate_api/GoogleTranslateAPI.java
+++ source/backend/src/main/java/com/workmates/backend/external_api/google_translate_api/GoogleTranslateAPI.java
@@ -5,38 +5,34 @@ import com.google.cloud.translate.v3.TranslateTextRequest;
 import com.google.cloud.translate.v3.TranslateTextResponse;
 import com.google.cloud.translate.v3.TranslationServiceClient;
 
-// 기능 방향
-// 이메일(최우선순위), 개인 메세지(DM), 포스트(게시글), 코멘트(댓글)에 적용 가능해야함
-// 실제 화면에서 텍스트의 국적이 같다면 번역이 적용되지 않고, 다르다면 적용할 수도, 적용하지 않을 수도 있음
-// 번역된 문장 <-> 번역 전 원문으로의 전환이 가능해야함
+public class GoogleTranslateApi {
 
-public class GoogleTranslateAPI {
+    // 구글 프로젝트 ID 및 위치(global 권장)
+    public static final String PROJECT_ID = "grounded-vision-467403-f8";
+    public static final String LOCATION = "global";
 
-    // 프로젝트 ID 및 위치(global 권장)
-    private static final String PROJECT_ID = "grounded-vision-467403-f8";
-    private static final String LOCATION = "global";
-
-    public static void main(String[] args) {
-        try (TranslationServiceClient client = TranslationServiceClient.create()) {
-
+    // 하나의 String에 묶인 문장(들)을 번역하고자 하는 언어코드에 맞게 일괄적으로 요청해서 하나의 String으로 반환
+    public static String getTranslatedText(final String originalText, final String targetLanguageCode) { 
+        try (TranslationServiceClient client = TranslationServiceClient.create()){
             LocationName parent = LocationName.of(PROJECT_ID, LOCATION);
-
-            String textToTranslate = "こんにちは";
-            String targetLanguage = "ko"; // 한국어
-
+        
             TranslateTextRequest request = TranslateTextRequest.newBuilder()
                     .setParent(parent.toString())
-                    .addContents(textToTranslate)
+                    .addContents(originalText)
                     .setMimeType("text/plain")
-                    .setTargetLanguageCode(targetLanguage)
+                    .setTargetLanguageCode(targetLanguageCode)
                     .build();
 
             TranslateTextResponse response = client.translateText(request);
 
             String translatedText = response.getTranslations(0).getTranslatedText();
-            System.out.println("Translated text: " + translatedText);
-        } catch (Exception e) {
+
+            return translatedText;
+
+        } catch(Exception e) {
             e.printStackTrace();
         }
+
+        return null;
     }
 }
