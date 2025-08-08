8c007bd36f77586a2c9adb357f47c067710edd8b
unknown <hwshin0727@gmail.com>
2025-08-08 18:21:24 +0900

    감성분석 api 작업중

--- source/backend/src/main/java/com/workmates/backend/domain/Comment.java
+++ source/backend/src/main/java/com/workmates/backend/domain/Comment.java
@@ -1,5 +1,7 @@
 package com.workmates.backend.domain;
 
+import java.time.LocalDateTime;
+
 import jakarta.persistence.Entity;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -15,6 +17,21 @@ import lombok.Setter;
 @AllArgsConstructor // 모든 필드를 매개변수로 받는 생성자
 public class Comment { // 댓글 엔티티
     
-    String content;
+    private Long id; // 댓글 식별 번호
 
+    private Long root_id; // 현재 댓글이 대댓글이라면 원댓글의 식별 번호
+
+    private Integer depth; // 댓글 깊이(현재 댓글이 원댓글이라면 깊이는 0)
+
+    private String content; // 댓글 내용
+
+    private LocalDateTime written_at; // 댓글 작성일시
+
+    private String attachment_url; // 댓글 첨부파일 url
+
+    private Boolean is_deleted; // 댓글 삭제 여부
+
+    private String writer_id; // 댓글 작성자 아이디
+
+    private Long post_id; // 댓글이 작성된 게시글 아이디
 }
