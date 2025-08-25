80a326ff877a8042a6a60ef5d68ca57a67f6cdd4
unknown <hwshin0727@gmail.com>
2025-08-25 18:21:53 +0900

    감성분석 api 작업중

--- source/backend/src/main/java/com/workmates/backend/BackendApplication.java
+++ source/backend/src/main/java/com/workmates/backend/BackendApplication.java
@@ -1,5 +1,6 @@
 package com.workmates.backend;
 
+import java.util.List;
 import java.util.concurrent.CompletableFuture;
 
 import org.springframework.boot.SpringApplication;
@@ -17,5 +18,19 @@ public class BackendApplication {
 		ConfigurableApplicationContext context = SpringApplication.run(BackendApplication.class, args);
 
 		OpenAiApiService service = context.getBean(OpenAiApiService.class);
+
+		List<String> testList = List.of("아주 흥미롭네요.", "전반적으로 시시했습니다.", "아기 거북이 알을 깨고 나오는 과정에서 생명의 신비를 느꼈습니다.", "영상미가 아주 뛰어납니다.", "금세기 최고의 자연 다큐멘터리라고 생각합니다.");
+
+		for(String s : testList) {
+			System.out.println(s);
+		}
+		
+		service.sentimentAnalysisAsync(testList)
+			.thenApply(res -> {
+				System.out.println("분석결과 : " + res);
+
+				return res;
+			})
+			.exceptionally(ex -> "감성분석에 실패했습니다.");
 	}
 }
--- source/backend/src/main/java/com/workmates/backend/external_api/open_ai_api/OpenAiApiService.java
+++ source/backend/src/main/java/com/workmates/backend/external_api/open_ai_api/OpenAiApiService.java
@@ -121,44 +121,55 @@ public class OpenAiApiService {
         });
     }
 
-    // public CompletableFuture<String> sentimentAnalysisAsync(List<Comment> commentList) throws Exception {
-    //     HttpClient client = HttpClient.newHttpClient();
-    //     ObjectMapper mapper = new ObjectMapper();
+    public CompletableFuture<String> sentimentAnalysisAsync(List<String> commentList) throws Exception {
+        HttpClient client = HttpClient.newHttpClient();
+        ObjectMapper mapper = new ObjectMapper();
 
-    //     ObjectNode messageObj = mapper.createObjectNode();
-    //     messageObj.put("role", "user");
-    //     messageObj.put("content", "Translate the following text from " + sourceLang + " to " + targetLang + ":\n" + text);
+        StringBuilder comments = new StringBuilder();
+        // commentList.sort(c1, c2 -> (int) (c2.getId() - c1.getId()));
+        for(int s = 0, e = Math.min(commentList.size(), 30); s < e; ++s) {
+            comments.append(String.valueOf(s + 1) + ". ");
+            comments.append(commentList.get(s));
+            comments.append(" ");
+        }
 
-    //     ArrayNode messagesArray = mapper.createArrayNode();
-    //     messagesArray.add(messageObj);
+        System.out.println("comments : " + comments.toString());
 
-    //     ObjectNode jsonBody = mapper.createObjectNode();
-    //     jsonBody.put("model", chatModel);
-    //     jsonBody.set("messages", messagesArray);
-    //     jsonBody.put("max_tokens", 100);
+        ObjectNode messageObj = mapper.createObjectNode();
+        messageObj.put("role", "user");
+        messageObj.put("content", comments.toString() + "앞서 주어진 문장들은 어떤 게시물에 대해 사용자들이 남긴 댓글의 리스트입니다. 댓글들의 전체적인 반응을 분석해서 요약하고, 댓글들이 작성된 언어에 맞게 번역해서 대답해주세요.\n");
 
-    //     HttpRequest request = HttpRequest.newBuilder()
-    //             .uri(URI.create(chatApiUrl))
-    //             .header("Content-Type", "application/json")
-    //             .header("Authorization", "Bearer " + getDecryptedApiKey())
-    //             .POST(HttpRequest.BodyPublishers.ofByteArray(mapper.writeValueAsBytes(jsonBody)))
-    //             .build();
 
-    //     return client.sendAsync(request, HttpResponse.BodyHandlers.ofString())
-    //         .thenApply(HttpResponse::body)
-    //         .thenApply(response -> {
-    //             try {
-    //                 JsonNode root = mapper.readTree(response);
-    //                 JsonNode choices = root.path("choices");
-    //                 if (choices.isArray() && choices.size() > 0) {
-    //                     JsonNode message = choices.get(0).path("message");
-    //                     return message.path("content").asText();
-    //                 } else {
-    //                     return null;
-    //                 }
-    //             } catch (Exception e) {
-    //                 throw new RuntimeException(e);
-    //             }
-    //     });
-    // }
+        ArrayNode messagesArray = mapper.createArrayNode();
+        messagesArray.add(messageObj);
+
+        ObjectNode jsonBody = mapper.createObjectNode();
+        jsonBody.put("model", chatModel);
+        jsonBody.set("messages", messagesArray);
+        jsonBody.put("max_tokens", 100);
+
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(chatApiUrl))
+                .header("Content-Type", "application/json")
+                .header("Authorization", "Bearer " + getDecryptedApiKey())
+                .POST(HttpRequest.BodyPublishers.ofByteArray(mapper.writeValueAsBytes(jsonBody)))
+                .build();
+
+        return client.sendAsync(request, HttpResponse.BodyHandlers.ofString())
+            .thenApply(HttpResponse::body)
+            .thenApply(response -> {
+                try {
+                    JsonNode root = mapper.readTree(response);
+                    JsonNode choices = root.path("choices");
+                    if (choices.isArray() && choices.size() > 0) {
+                        JsonNode message = choices.get(0).path("message");
+                        return message.path("content").asText();
+                    } else {
+                        return null;
+                    }
+                } catch (Exception e) {
+                    throw new RuntimeException(e);
+                }
+        });
+    }
 }
