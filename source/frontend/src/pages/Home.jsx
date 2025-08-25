9f7d5d400df7b304da4216ec1b81be864ca9f291
YSY <sddmjn51@gmail.com>
2025-08-25 14:10:53 +0900

    2028.08.25_header
    2028.08.25_header

--- source/frontend/src/pages/Home.jsx
+++ source/frontend/src/pages/Home.jsx
@@ -1,21 +1,16 @@
 // src/pages/Home.jsx
 
+import Header from '../Components/Header';
 import AuthButtons from '../Components/AuthButtons';
 import ChatroomButton from '../Components/ChatroomButton';
 import Chatroom from './Chatroom';
 
 const Home = () => {
   return (
-    <div className="min-h-screen bg-gray-50">
-      {/* 상단 바 */}
-      <header className="bg-white shadow-sm border-b">
-        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-          <div className="flex justify-between items-center h-16">
-            <h1 className="text-xl font-bold text-gray-900">🏠 Workmates</h1>
-            <AuthButtons />
-          </div>
-        </div>
-      </header>
+    <div className="min-h-screen bg-gray-50 pt-[80px]"> {/* pt-[100px]: Header height 만큼 padding top */}
+
+    {/* 상단바 영역 */}
+    <Header />
 
       {/* 본문 영역 (비워둠) */}
       <main className="p-8 text-center text-gray-400">
