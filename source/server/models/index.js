d7af193b04c9d840a84b4fdde4c36e52d52e77b0
bangzta <kofqudtn93@gmail.com>
2025-08-28 16:08:31 +0900

    추가

--- source/server/models/index.js
+++ source/server/models/index.js
@@ -1,16 +1,17 @@
-const { Sequelize, DataTypes } = require('sequelize');
-const sequelize = require('../config/database');
+const { Sequelize, DataTypes } = require("sequelize");
+const sequelize = require("../config/database");
 
 const db = {};
 db.Sequelize = Sequelize;
 db.sequelize = sequelize;
 
 // 모델 등록
-db.User = require('./user')(sequelize, DataTypes);
-db.Email = require('./email')(sequelize, DataTypes);
+db.User = require("./user")(sequelize, DataTypes);
+db.Email = require("./email")(sequelize, DataTypes);
+db.Friend = require("./friend")(sequelize, DataTypes);
 
 // 관계 연결
-Object.keys(db).forEach(modelName => {
+Object.keys(db).forEach((modelName) => {
   if (db[modelName].associate) {
     db[modelName].associate(db);
   }
