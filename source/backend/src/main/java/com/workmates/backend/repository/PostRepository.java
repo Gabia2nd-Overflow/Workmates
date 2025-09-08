a7c75632c8c25bab7c3f890587694535baaf1946
gyugyuluck <136181516+gyugyuluck@users.noreply.github.com>
2025-09-08 10:44:39 +0900

    페이징
    ㅇㅇ

--- source/backend/src/main/java/com/workmates/backend/repository/PostRepository.java
+++ source/backend/src/main/java/com/workmates/backend/repository/PostRepository.java
@@ -3,6 +3,8 @@ package com.workmates.backend.repository;
 
 import java.util.List;
 import java.util.Optional;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -13,4 +15,6 @@ public interface PostRepository extends JpaRepository<Post, Long> {
     List<Post> findByThreadIdAndIsDeletedFalseOrderByCreatedAtDesc(Long threadId);
 
     Optional<Post> findByIdAndIsDeletedFalse(Long id);
+
+    Page<Post> findAllByCategoryContainingIgnoreCase(String category, Pageable pageable);
 }
