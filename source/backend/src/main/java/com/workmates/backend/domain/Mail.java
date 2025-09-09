2ffe9663ec89ad189afd9ff8b880b4b35c9380c2
hwShin0727 <hwshin0727@gmail.com>
2025-09-09 10:37:40 +0900

    feat(mail): 메일 송수신 API 작업 시작

---
+++ source/backend/src/main/java/com/workmates/backend/domain/Mail.java
@@ -0,0 +1,44 @@
+package com.workmates.backend.domain;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "mail",
+    indexes = {
+        @Index(name = "idx_from", columnList = "from"),
+        @Index(name = "idx_to", columnList = "to"),
+        @Index(name = "idx_user_id", columnList = "user_id")
+    }
+)
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Mail {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private Long id;
+    
+    @Column(name = "from", nullable = false)
+    private String from;
+    
+    @Column(name = "to", nullable = false)
+    private String to;
+    
+    @Column(name = "subject", nullable = false)
+    private String subject;
+    
+    @Column(name = "content")
+    private String content;
+    
+    @Column(name = "send_date", nullable = false)
+    private LocalDateTime sendDate;
+        
+    @Column(name = "user_id", nullable =  false)
+    private String userId;  // 메일을 송수신한 사용자 아이디
+}
