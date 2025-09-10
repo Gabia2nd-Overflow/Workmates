cd8b0485006ce949c3e427e75984d3ab31733086
Seyoung <sddmjn51@gmail.com>
2025-09-10 17:57:50 +0900

    디자인 변경, MyWorkshopsPanel 추가

--- source/backend/src/main/java/com/workmates/backend/web/controller/MailController.java
+++ source/backend/src/main/java/com/workmates/backend/web/controller/MailController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.workmates.backend.service.MailService;
 
 import lombok.RequiredArgsConstructor;
-
+/* 
 @RestController
 @RequestMapping("/api/mail")
 @RequiredArgsConstructor
@@ -21,3 +21,4 @@ public class MailController {
     @PostMapping("/send")
     
 }
+    */
